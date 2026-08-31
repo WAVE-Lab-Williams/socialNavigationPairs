6f3693bfbd917518557fe90399b9d8b65ac0fd19
Kim W. Wong <kimberly.wong@yale.edu>
2026-08-30 22:28:01 -0400

    - prepares first full functional version of socialNavigationPairs, schema setup, unkey set up, and vercel backend setup. - fixes an across-trial lingering cursor issue in two ways: 1) updates styles.css to make caret color transparent 2) deals with possible carryover from autofocused input in consent form/id input/every single trial refresh. - returns params to all true - clears notebook metadata.

--- src/js/core/trial.js
+++ src/js/core/trial.js
@@ -37,6 +37,12 @@ function runSingleTrial(
         type: jsPsychCallFunction,
         func: function () {
             document.body.style.cursor = 'none';
+            if (document.activeElement && document.activeElement.blur) {
+                document.activeElement.blur();
+            }
+            if (window.getSelection) {
+                window.getSelection().removeAllRanges();
+            }
         },
     };
 
